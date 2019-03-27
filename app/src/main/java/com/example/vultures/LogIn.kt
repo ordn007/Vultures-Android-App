df2954e27d369d44c65e108ec4a5c6109fd7637a
Azam Abidjanov <azam.abidjanov@gmail.com>
2019-03-27 12:42:38 -0600

    Added Google/Email sign-in methods

--- app/src/main/java/com/example/vultures/LogIn.kt
+++ app/src/main/java/com/example/vultures/LogIn.kt
@@ -1,5 +1,7 @@
 package com.example.vultures
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,14 +9,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LogIn : AppCompatActivity() {
 
-
     companion object {
         private const val LOG_TAG = "448.LogIn"
         private const val SIGNUP = 1
+        private const val RC_SIGN_IN = 123
     }
 
 
@@ -35,6 +40,63 @@ class LogIn : AppCompatActivity() {
         login_password_field.text=null
     }
 
+    private fun createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+          //  AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+           // AuthUI.IdpConfig.FacebookBuilder().build(),
+           // AuthUI.IdpConfig.TwitterBuilder().build())
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.vultureaid_icon)      // Set logo drawable
+                .setTheme(R.style.AppTheme)
+                .build(),
+            RC_SIGN_IN)
+        // [END auth_fui_create_intent]
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(applicationContext, "Sucesfully signed in", Toast.LENGTH_SHORT).show()
+                // Successfully signed in
+                signOut()
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(applicationContext, user?.email.toString(), Toast.LENGTH_SHORT).show()
+
+
+                // ...
+            } else {
+                Toast.makeText(applicationContext, "Sign in failed", Toast.LENGTH_SHORT).show()
+
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    private fun signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                // ...
+            }
+        // [END auth_fui_signout]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,6 +121,9 @@ class LogIn : AppCompatActivity() {
 
         }
         login_btn_reset.setOnClickListener { cleanUpTextFields() }
+        google_sign_in.setOnClickListener {
+            createSignInIntent()
+        }
     
     }
 
